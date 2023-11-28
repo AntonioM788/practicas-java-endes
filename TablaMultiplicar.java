0631ba0ea2c4528ff4cb5a8e4406d914ff137f3d
AntonioM788 <145494163+AntonioM788@users.noreply.github.com>
2023-11-28 11:51:24 +0100

    Create TablaMultiplicar.java

---
+++ TablaMultiplicar.java
@@ -0,0 +1,64 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class TablaMultiplicar
+ */
+public class TablaMultiplicar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TablaMultiplicar() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String numero1 = request.getParameter("numero1");
+		int tabla = 0;
+		
+		 
+		 PrintWriter out = response.getWriter(); //esto es para que me lo saque por pantalla
+		 
+			 Integer n1 = Integer.parseInt(numero1);//puedo escribir una variable nueva
+
+
+		/* }catch(Exception error) {//Esto es para parsear de un String a un entero
+			 System.out.println(error.toString());
+		 }*/
+		 
+		 for (int i = 1; i <=10; i++) {
+			 
+			 tabla = i*n1;
+			 out.print("La tabla de multiplicar es: "+tabla);
+			
+		}
+		
+		
+		
+	}
+
+		
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
